3519f2df89d85ce7dd96bbb2507730983a26b4dd
Alberto Guirado Méndez <al_aguiradom@iessierradelasvillas.org>
2022-10-10 10:24:14 +0200

    reto spiderman a medias

---
+++ Retospierman.py
@@ -0,0 +1,27 @@
+#Camino mas rapido para las emergencias de spiderman
+
+spiderman = 0
+camino1 = 0
+camino2 = 0
+
+print(" Suponiendo que spiderman esta en el punto 0:")
+camino1= int (input("donde está la primrea emergencia:\n"))
+Camino2= int (input("donde está la segunda emergencia:\n"))
+
+if ((camino1>0) and (Camino2 >0)):
+    if(camino1>Camino2):
+        print("El camino mas corto sería de : ", camino1 "Km" )
+    else (camino1>camino1):
+        print("El camino mas corot es de: ", Camino2 "Km")
+
+if (camino1 and Camino2 <0):
+    if(camino1>Camino2):
+        print("El camino mas corto sería de : ", (-camino1) "Km" )
+    else (camino1>Camino2):
+        print("El camino mas corot sería de: ", (-Camino2) "Km")
+
+if (camino1>0)(Camino2<0):
+    if(camino1>Camino2):
+        print("El camino mas corto sería de : ", ((camino1*2)+ Camino2) "Km" )
+    else (camino1>camino2):
+        print("El camino mas corto sería de: ", (Camino2) "Km")
